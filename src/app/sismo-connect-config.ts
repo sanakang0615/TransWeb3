4289605ad5f89147799336ac047219485a7ae52e
sanakang0615 <sanakang0615@kaist.ac.kr>
2023-09-01 15:04:32 +0900

    Delete: unnecessary code lines

--- src/app/sismo-connect-config.ts
+++ src/app/sismo-connect-config.ts
@@ -46,13 +46,6 @@ export const AUTHS: AuthRequest[] = [
 
 // Request users to prove membership in a Data Group (e.g I own a wallet that is part of a DAO, owns an NFT, etc.)
 export const CLAIMS: ClaimRequest[] = [
-  // {
-  //   // claim on Sismo Hub GitHub Contributors Data Group membership: https://factory.sismo.io/groups-explorer?search=0xda1c3726426d5639f4c6352c2c976b87
-  //   // Data Group members          = contributors to sismo-core/sismo-hub
-  //   // value for each group member = number of contributions
-  //   // request user to prove membership in the group
-  //   groupId: "0xda1c3726426d5639f4c6352c2c976b87", // impersonated github:dhadrien has 1 contribution, eligible
-  // },
   {
     // claim Gitcoin Passport Holders Data Group membership: https://factory.sismo.io/groups-explorer?search=0x1cde61966decb8600dfd0749bd371f12
     // Data Group members          = Gitcoin Passport Holders
@@ -62,31 +55,11 @@ export const CLAIMS: ClaimRequest[] = [
     claimType: ClaimType.GTE,
     value: 8, // dhadrien.sismo.eth has a score of 46, eligible. Can reveal more.
     isSelectableByUser: true, // can reveal more than 15 if they want
-  },
-  // {
-  //   // claim ENS DAO Voters Data Group membership: https://factory.sismo.io/groups-explorer?search=0x85c7ee90829de70d0d51f52336ea4722
-  //   // Data Group members          = voters in ENS DAO
-  //   // value for each group member = number of votes in ENS DAO
-  //   // request user to prove membership in the group with value >= 17
-  //   groupId: "0x85c7ee90829de70d0d51f52336ea4722",
-  //   claimType: ClaimType.GTE,
-  //   value: 4, // impersonated dhadrien.sismo.eth has 17 votes, eligible
-  //   isSelectableByUser: true,
-  // },
-  // {
-  //   // claim on Stand with Crypto NFT Minters Data Group membership: https://factory.sismo.io/groups-explorer?search=0xfae674b6cba3ff2f8ce2114defb200b1
-  //   // Data Group members          = minters of the Stand with Crypto NFT
-  //   // value for each group member = number of NFT minted
-  //   // request user to prove membership in the group with value = 10
-  //   groupId: "0xfae674b6cba3ff2f8ce2114defb200b1",
-  //   claimType: ClaimType.EQ,
-  //   value: 10, // dhadrin.sismo.eth minted exactly 10, eligible
-  //   isOptional: true,
-  // },
+  }
 ];
 
 // Request users to sign a message
 export const SIGNATURE_REQUEST: SignatureRequest = {
   message: "TransWeb3 계정을 생성하는 것에 동의하겠습니다.",
-  isSelectableByUser: true,
+  isSelectableByUser: false,
 };
