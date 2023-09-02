ce62d88418b60b14ffa73839dfc4f5854b0530fe
sanakang0615 <sanakang0615@kaist.ac.kr>
2023-09-02 18:30:38 +0900

    Add: more wallets by "Wagmi"

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,8 +4,14 @@ import { Layout } from "antd";
 import { Inter } from "next/font/google";
 import { WagmiConfig, createConfig, configureChains } from 'wagmi'
 import { sepolia, taikoTestnetSepolia, baseGoerli, foundry, polygon, optimism, mainnet } from 'wagmi/chains'
+
+import { alchemyProvider } from 'wagmi/providers/alchemy'
 import { publicProvider } from 'wagmi/providers/public'
+
+import { CoinbaseWalletConnector } from 'wagmi/connectors/coinbaseWallet'
+import { InjectedConnector } from 'wagmi/connectors/injected'
 import { MetaMaskConnector } from 'wagmi/connectors/metaMask'
+import { WalletConnectConnector } from 'wagmi/connectors/walletConnect'
 
 
 const inter = Inter({ subsets: ["latin"] });
@@ -16,7 +22,27 @@ const { chains, publicClient, webSocketPublicClient } = configureChains(
 const config = createConfig({
   autoConnect: true,
   connectors: [
-    new MetaMaskConnector({ chains })],
+    new MetaMaskConnector({ chains }),
+    new CoinbaseWalletConnector({
+      chains,
+      options: {
+        appName: 'wagmi',
+      },
+    }),
+    new WalletConnectConnector({
+      chains,
+      options: {
+        projectId: '...',
+      },
+    }),
+    new InjectedConnector({
+      chains,
+      options: {
+        name: 'Injected',
+        shimDisconnect: true,
+      },
+    }),
+  ],
   publicClient,
   webSocketPublicClient,
 })
