5dd900bb973c637da21558c29d0357216b9a15f3
sanakang0615 <sanakang0615@kaist.ac.kr>
2023-09-02 21:03:58 +0900

    Fix: the location of Header component

--- src/app/authCheck/page.tsx
+++ src/app/authCheck/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import Header from "./components/Header";
+import Header from "../components/Header.tsx";
 import {
   SismoConnectButton,
   SismoConnectResponse,
